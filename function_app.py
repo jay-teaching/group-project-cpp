17da312bc4f426283d7b10c3e88e3ec93ce28f29
isabel0803 <66700465+isabel0803@users.noreply.github.com>
2025-12-10 16:38:06 +0000

    build: edit function prediction

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+from prediction import make_prediction
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -7,19 +8,20 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def http_trigger_predict_cpp_group(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    tenure = req.params.get('tenure')
+    monthly = req.params.get('monthly')
+    techsupport = req.params.get('techsupport')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    prediction = make_prediction(
+        tenure=tenure,
+        MonthlyCharges=monthly,
+        TechSupport_yes=techsupport
+    )
+
+    if tenure and monthly and techsupport:
+        return func.HttpResponse(f"Hello, the probability of churn is {prediction}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a valid tenure, monthly and techsupport in the query string or in the request body for a personalized response.",
              status_code=200
         )
